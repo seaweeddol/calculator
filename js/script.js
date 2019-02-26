fb60db3c21640f1da38c11c8afc8edce77619c62
seaweeddol <jstone4815@gmail.com>
2019-02-26 15:26:18 -0800

    Set up variables and button event listener

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+const addButton = document.querySelector('#add');
+const subtractButton = document.querySelector('#subtract');
+const multiplyButton = document.querySelector('#multiply');
+const divideButton = document.querySelector('#divide');
+const clearButton = document.querySelector('#clear');
+const numButtons = document.querySelectorAll('.number');
+const display = document.querySelector('.display');
+let result = '';
+
+numButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    result += button.textContent;
+    display.textContent = result;
+  })
+})
+
 function add(a, b){
   return a + b;
 }
@@ -12,4 +28,16 @@ function multiply(a, b) {
 
 function divide(a, b){
   return a / b;
+}
+
+function operate(operator, a, b){
+  if(operator == '+'){
+    return add(a, b);
+  } else if(operator == '-'){
+    return subtract(a, b);
+  } else if(operator == '*'){
+    return multiply(a, b);
+  } else if(operator == '/'){
+    return divide(a, b);
+  }
 }
